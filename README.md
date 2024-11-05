6ed6023fcbc84c1c44cbc55843ad3191b00e419f
Aatrick <113598245+Aatrick@users.noreply.github.com>
2024-11-05 19:31:59 +0100

    Update README and main.py for vibrator control application; add installation instructions and improve code formatting

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # BLXinput
+
 Bluetooth xbox vibrator controller
+
+## Description
+
+Simple python script to control the vibration of an xbox controller connected via usb or bluetooth, it uses the XInput library to communicate with the controller. It can be used for anything that requires a vibration feedback, like games, notifications, etc. ( ͡° ͜ʖ ͡°)
+
+![alt text](image.png)
+
+## Installation
+
+Simply clone the repository and install the required dependencies with pip:
+
+`pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import XInput 
+import XInput
 import customtkinter
 import random
 
@@ -14,7 +14,7 @@ print(XInput.get_battery_information(index))
 def randomize():
     l_slider.set(random.randint(0, 100))
     r_slider.set(random.randint(0, 100))
-    XInput.set_vibration(index, l_slider.get()/100, r_slider.get()/100)
+    XInput.set_vibration(index, l_slider.get() / 100, r_slider.get() / 100)
     print(f"Left: {l_slider.get()}, Right: {r_slider.get()}")
 
 
@@ -25,20 +25,32 @@ app.title("Vibrator Control")
 title = customtkinter.CTkLabel(app, text="Vibrator Control")
 title.grid(row=0, column=1)
 
-#make two vertical sliders side by side
+# make two vertical sliders side by side
 l_slider = customtkinter.CTkSlider(app, from_=0, to=100, orientation="vertical")
-l_labl = customtkinter.CTkLabel(app, text="Left Vibrator")# place the label under the left slider
+l_labl = customtkinter.CTkLabel(
+    app, text="Left Vibrator"
+)  # place the label under the left slider
 l_slider.grid(row=1, column=0, padx=50)
 l_labl.grid(row=2, column=0, padx=50)
 
-l_slider.bind("<ButtonRelease-1>", lambda event: XInput.set_vibration(index, l_slider.get()/100, r_slider.get()/100))
+l_slider.bind(
+    "<ButtonRelease-1>",
+    lambda event: XInput.set_vibration(
+        index, l_slider.get() / 100, r_slider.get() / 100
+    ),
+)
 
 r_slider = customtkinter.CTkSlider(app, from_=0, to=100, orientation="vertical")
 r_labl = customtkinter.CTkLabel(app, text="Right Vibrator")
 r_slider.grid(row=1, column=2, padx=50)
 r_labl.grid(row=2, column=2, padx=50)
 
-r_slider.bind("<ButtonRelease-1>", lambda event: XInput.set_vibration(index, l_slider.get()/100, r_slider.get()/100))
+r_slider.bind(
+    "<ButtonRelease-1>",
+    lambda event: XInput.set_vibration(
+        index, l_slider.get() / 100, r_slider.get() / 100
+    ),
+)
 
 random_button = customtkinter.CTkButton(app, text="Random", command=lambda: randomize())
 random_button.grid(row=3, column=1)
